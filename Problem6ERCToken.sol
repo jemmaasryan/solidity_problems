2fb71db6b0976aa1e28702e2259d6f1173867b60
jemmaasryan <92854542+jemmaasryan@users.noreply.github.com>
2022-12-19 16:04:37 +0400

    Create Problem6ERCToken.sol

---
+++ Problem6ERCToken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Yerevan is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("Yerevan", "YVN") {}
+}
