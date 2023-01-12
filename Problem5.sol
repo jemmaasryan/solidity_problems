c149fde8ee3eb96e1aeec8d255b6584dcf442775
jemmaasryan <92854542+jemmaasryan@users.noreply.github.com>
2023-01-12 13:40:15 +0400

    Add Problem5.sol

---
+++ Problem5.sol
@@ -0,0 +1,45 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.15;
+/*Ստեղծել կոնտրակտ, որը constructor-ի միջոցով կարժեքավորի address տիպի
+  owner փոփոխականը հետագայում onlyOwner modifier-ը օգտագործելու համար։ 
+  Owner-ը որևիցե ֆունկցիայի միջոցով հնարավորություն պիտի ունենա ավելացնել բաժնետերերի
+  այսինքն՝ address և percentage յուրաքանչյուրի համար (ավելացնել նաև բաժնետեր 
+  հեռացնելու հնարավորությունը)։ Contract-ը հնարավորություն պիտի ունենա իր մեջ 
+  գումար պահելու (receive) և ամեն անգամ երբ որոշակի գումար կփոխանցվի contract-ի 
+  հասցեին, contract-ը կուղարկի այն բաժնետերերին իրենց տոկոսներին համապատասխան։  
+*/
+contract Problem5 {
+    address public owner;
+   
+    struct Shareholder {
+        address personAddress;
+        uint percentage;
+    }
+    
+    mapping(address => uint) public shareholder;
+    mapping(address => Shareholder) public system;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not allowed.");
+        _;
+    }
+
+    function addShareholder(address _shareholder, uint _percentage) external onlyOwner {
+        system[msg.sender].personAddress = _shareholder;
+        system[msg.sender].percentage = _percentage;
+    }
+
+    function removeShareholder(address _shareholder) external onlyOwner {
+        system[_shareholder].personAddress = address(0);
+        system[_shareholder].percentage = 0;
+    }
+
+    receive() external payable {
+
+    }
+}
