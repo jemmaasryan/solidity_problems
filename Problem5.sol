58713e6566e98b000db7ebe7b4165b68aabc7860
jemmaasryan <92854542+jemmaasryan@users.noreply.github.com>
2023-01-13 10:39:19 +0400

    Update Problem5.sol

--- Problem5.sol
+++ Problem5.sol
@@ -1,6 +1,6 @@
 //SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.15;
+pragma solidity ^0.8.15;
 /*Ստեղծել կոնտրակտ, որը constructor-ի միջոցով կարժեքավորի address տիպի
   owner փոփոխականը հետագայում onlyOwner modifier-ը օգտագործելու համար։ 
   Owner-ը որևիցե ֆունկցիայի միջոցով հնարավորություն պիտի ունենա ավելացնել բաժնետերերի
@@ -10,16 +10,17 @@ pragma solidity 0.8.15;
   հասցեին, contract-ը կուղարկի այն բաժնետերերին իրենց տոկոսներին համապատասխան։  
 */
 contract Problem5 {
-    address public owner;
+    address payable public owner;
    
     struct Shareholder {
         address personAddress;
         uint percentage;
+        bool exists;
     }
-    
     mapping(address => uint) public shareholder;
     mapping(address => Shareholder) public system;
     Shareholder[] public all;
+
     constructor() {
         owner = payable(msg.sender);
     }
@@ -30,11 +31,11 @@ contract Problem5 {
     }
 
     function addShareholder(address _shareholder, uint _percentage) external onlyOwner {
-        require(system[_shareholder].exists, "Already in system.");
+        require(!system[_shareholder].exists, "Already in system.");
         system[msg.sender].personAddress = _shareholder;
         system[msg.sender].percentage = _percentage;
         shareholder[_shareholder] = _percentage;
-        all.push(Shareholder(_shareholder, _percentage, false));
+        all.push(Shareholder(_shareholder, _percentage, true));
     }
 
     function removeShareholder(address _shareholder) external onlyOwner {
@@ -42,8 +43,16 @@ contract Problem5 {
         delete system[msg.sender].personAddress;
         system[msg.sender].percentage = 0;
     }
-
+    function shares(uint256 amount) external payable {
+        uint256 total = 0;
+        for(uint256 i = 0; i < all.length; i++) {
+            if(all[i].exists) {
+                total += all[i].percentage;
+                uint256 share = (amount * all[i].percentage) / total;
+            }
+        }
+    }
     receive() external payable {
-        
+
     }
 }
